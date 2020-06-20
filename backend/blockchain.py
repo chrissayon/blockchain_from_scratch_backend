01b4f36aca7c96edef1343222f413a18b8e67ff7
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2020-06-21 09:41:31 +1000

    Seperated block and added __main__

--- backend/blockchain.py
+++ backend/blockchain.py
@@ -1,13 +1,4 @@
-class Block:
-    """
-    Block: a unit of storage
-    Store transactions in a blockchain that supports a cryptocurrency
-    """
-    def __init__(self, data):
-        self.data = data
-
-    def __repr__(self):
-        return f"Block - data: {self.data}"
+from block import Block
 
 
 class Blockchain:
@@ -25,8 +16,15 @@ class Blockchain:
         return f"Blockchain: {self.chain}"
 
 
-blockchain = Blockchain()
-blockchain.add_block('one')
-blockchain.add_block('two')
+def main():
+    blockchain = Blockchain()
+    blockchain.add_block('one')
+    blockchain.add_block('two')
 
-print(blockchain)
+    print(blockchain)
+
+    print(f'block.py __name__: {__name__}')
+
+
+if __name__ == '__main__':
+    main()
