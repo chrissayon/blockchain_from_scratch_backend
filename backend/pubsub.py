512a043d06c4ee7165260b006d33f3eebaf00d92
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2020-06-22 20:52:14 +1000

    Turned pubsub into a class for reuse

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -9,28 +9,44 @@ from pubnub.callbacks import SubscribeCallback
 pnconfig = PNConfiguration()
 pnconfig.publish_key = os.environ.get('pubnub_publish_key')
 pnconfig.subscribe_key = os.environ.get('pubnub_subscribe_key')
-pubnub = PubNub(pnconfig)
 
 TEST_CHANNEL = 'TEST_CHANNEL'
-
-# Tells that pubnub is now subscribed to all designated channels
-pubnub.subscribe().channels([TEST_CHANNEL]).execute()
 
 
 class Listener(SubscribeCallback):
     def message(self, pubnub, message_object):
-        print(f'\n-- Incoming message_object: {message_object}')
+        print(f'\n-- Channel: {message_object.channel} \n\
+            Message: {message_object.message}')
 
 
-# Deals with what happens when packet received
-pubnub.add_listener(Listener())
+class PubSub():
+    """
+    Handles the publish and subscribe layer of the application
+    Provides the communication between the ndoes of the blockchain network
+    """
+    def __init__(self):
+        self.pubnub = PubNub(pnconfig)
+
+        # Tells that pubnub is now subscribed to all designated channels
+        self.pubnub.subscribe().channels([TEST_CHANNEL]).execute()
+
+        # Deals with what happens when packet received
+        self.pubnub.add_listener(Listener())
+
+    def publish(self, channel, message):
+        """
+        Publish the message to the channel.
+        """
+        self.pubnub.publish().channel(channel).message(message).sync()
 
 
 # We need to overwrite channel to publish to
 def main():
+    pubsub = PubSub()
+
     time.sleep(1)
 
-    pubnub.publish().channel(TEST_CHANNEL).message({'foo': 'bar'}).sync()
+    pubsub.publish(TEST_CHANNEL, {'foo': 'bar'})
 
 
 if __name__ == '__main__':
