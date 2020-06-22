47d13ab44d8e655abf788d99d4d4ee9fa4616641
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2020-06-22 21:05:17 +1000

    Added multi-instance functionality for ports

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,3 +1,6 @@
+import os
+import random
+
 from flask import Flask, jsonify
 
 from backend.blockchain.blockchain import Blockchain
@@ -27,4 +30,10 @@ def route_blockchain_mine():
     return jsonify(blockchain.chain[-1].to_json())
 
 
-app.run()
+# If instance running on port 5000 already, switch to a different port
+PORT = 5000
+
+if os.environ.get('PEER') == 'True':
+    PORT = random.randint(5001, 6000)
+
+app.run(port=PORT)
